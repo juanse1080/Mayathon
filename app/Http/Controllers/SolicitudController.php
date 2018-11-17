fadce27e96d241d81c466c2174ac6ed701b42371
piiok <33143055+piiok@users.noreply.github.com>
2018-11-17 11:25:01 -0500

    nueva funcion

--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -187,6 +187,120 @@ class SolicitudController extends Controller {
         ]);        
     }
 
+    //Util para el seeder
+    private function creadorRiesgo($usuario,$solicitud){
+        if($usuario->empresa){
+            $tipo=0;
+            if($edad>1){
+                if($edad>3){
+                    $tiempo=0;
+                }else{
+                    $tiempo=1;
+                }
+            }else{
+                $tiempo=1;
+            }
+        }else{
+            $tipo=1;
+            if($edad>30 & $edad<60){
+                $tiempo=0;
+            }else{
+                $tiempo=1;
+            }
+            
+        }
+        if($usuario->activos > $solicitud->monto_requerido){
+            $act=0;
+        }else{
+            if($usuario->activos < ($solicitud->monto_requerido/2)){
+                $act=1;
+            }else{
+                $act=0;
+            }
+            
+        }
+        switch ($solicitud->categoria) {
+            case "educacion":
+                $cat=1;
+                break;
+            case "investigacion":
+                $cat=0;
+                break;
+            case "arte":
+                $cat=1;
+                break;
+            case "empresa":
+                $cat=0;
+                break;
+            case "personal":
+                $cat=1;
+                break;
+        }
+
+
+        if($usuario->pasivos == 0 or $usuario->activos==0){
+            $pa=1;
+        }else{
+            $pa=0;
+        }
+        if(strlen($solicitud->descripcion)>180){
+            $des=0;
+        }else{
+            $des=1;
+        }
+        if($solicitud->foto==null){
+            $fot=1;
+        }else{
+            $fot=0;
+        }
+        if($solicitud->video==null){
+            $vid=1;
+        }else{
+            $vid=0;
+        }
+        switch ($usuario->nivel) {
+            case "Ninguno":
+                $niv=3;
+                break;
+            case "Primaria":
+                $niv=3;
+                break;
+            case "Bachiller":
+                $niv=2;
+                break;
+            case "Tecnico":
+                $niv=1;
+                break;
+            case "Tecnologo":
+                $niv=1;
+                break;
+            case "Universitario":
+                $niv=0;
+                break;
+            case "Maestria":
+                $niv=0;
+                break;
+            case "Doctorado":
+                $niv=0;
+                break;
+            default:
+                $niv=0;
+        }
+
+        if($solicitud->monto_requerido>1000000){
+            if($solicitud->monto_requerido>100000000){
+                $mon=2;
+            }else{
+                $mon=1;
+            }
+        }else{
+            $mon=0;
+        }
+        $variables = [$tiempo,$tipo,$act,$pa,$pa,$cat,$des,$fot,$vid,$niv,$mon];
+        // dd($variables);
+        return $this->calcular_var($variables);
+    }
+
     /**
      * Display the specified resource.
      *
