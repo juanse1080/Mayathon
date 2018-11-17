384ec2c9a2d5c5d6afd07466373f2e30dd5a5286
juanse1080 <juanmarcon1080@gmail.com>
2018-11-17 01:19:41 -0500

    notificaciones

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,34 +2,17 @@
 
 namespace App\Console;
 
-use App\Solicitud;
-use App\Notificacion;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel {
     
     protected $commands = [
-        //
+        Commands\notificacionSolicitante::class,
     ];
 
     protected function schedule(Schedule $schedule){
-        $schedule->call(function () {
-            $solicitudes = new Solicitud;
-            $solicitudes = $solicitudes->where('estado', true)->get();
-            $dia = date('Y-m-d');
-            foreach ($solicitudes as $value) {
-                if($value->tiempo_recaudacion < $dia){
-                    Notificacion::create([
-                        'fk_usuario' => $value->fk_usuario,
-                        'estado' => false,
-                        'titulo' => '¡Su solicitud expiro!',
-                        'url' => '/solicitudes/'.$value->pk_solicitud,
-                        'descripcion' => 'Su solicitud ha expirado el tiempo especificado, por favor confirme el estado de su solicitud'
-                    ]);
-                }
-            }
-        })->everyMinute();
+        $schedule->command('notificacion:solicitante')->everyMinute();
     }
 
     protected function commands()
