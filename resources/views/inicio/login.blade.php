e8fc61716acdc62f4c12ec696ce30bea3d5d3edf
eduardcaballero <33143059+eduardcaballero@users.noreply.github.com>
2018-11-17 02:18:50 -0500

    login

--- resources/views/inicio/login.blade.php
+++ resources/views/inicio/login.blade.php
@@ -3,37 +3,37 @@
 @section('content')
 
 <section class="login-block">
-  <div class="container2">
-    <div class="row">
-      <div class="col-md-4 login-sec">
-          <h2 class="text-center">Acceso a ColCief</h2>
-          @include('error.login')
-          <form class="login-form" action="{{route('authenticate')}}" method="POST">
-            @csrf
-            <div class="form-group">
-              <label for="username" class="text-uppercase">Usuario</label>
-              <input type="text" class="form-control" placeholder="" name='username' id='username' value="{{old('username')}}">
-            </div>
-            <div class="form-group">
-              <label for="password" class="text-uppercase" >Contraseña</label>
-              <input type="password" class="form-control" placeholder="" name='password' id='password'>
-            </div>
-
-            <div class="form-check">
-              <button type="submit" class="btn btn-login float-right">Iniciar Sesión</button>
-            </div>
-            <br><br>
-            <div class="form-check ">
-                    <a class="float-right" href="/usuarios/crear">Registrarse</a>
-                </div>
-            </div>
+    <div class="container2">
+      <div class="row">
+        <div class="col-md-4 login-sec">
+            <h2 class="text-center">Acceso a MyLend</h2>
+            @include('error.login')
+            <form class="login-form" action="{{route('authenticate')}}" method="POST">
+              @csrf
+              <div class="form-group">
+                <label for="username" class="text-uppercase">Usuario</label>
+                <input type="text" class="form-control" placeholder="" name='username' id='username' value="{{old('username')}}">
+              </div>
+              <div class="form-group">
+                <label for="password" class="text-uppercase" >Contraseña</label>
+                <input type="password" class="form-control" placeholder="" name='password' id='password'>
+              </div>
+              <div class="form-group">
+                  <label for="reg" class="text-uppercase" ></label>
+                  <a  name="reg" class="float-right" href="/usuarios/crear">Registrarse</a>
+              </div>
+              <div class="form-check">
+                <button type="submit" class="btn btn-login float-right">Iniciar Sesión</button>
+              </div>
+              
+                
+            </form>
             
-          </form>
+        </div>
+        <div class="col-md-8 banner-sec"></div>
       </div>
-      <div class="col-md-8 banner-sec"></div>
     </div>
-  </div>
-
-</section>
+  
+  </section>
 
 @endsection
