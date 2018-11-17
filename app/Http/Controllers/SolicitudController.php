8999b420bfcb9629acbeb6f1209cedc9b4a6d83c
juanse1080 <juanmarcon1080@gmail.com>
2018-11-17 04:33:10 -0500

    funcion peso

--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -9,23 +9,15 @@ use App\Http\Controllers\UsuarioController;
 use App\Solicitud;
 use App\Multimedia;
 
-class SolicitudController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('usuarios.solicitudes');
+class SolicitudController extends Controller {
+
+    public function index(){
+        $solicitudes = Solicitud::where('solicitud.fk_usuario',session('datos')['pk_usuario'])->where('multimedia.tipo','foto')->leftjoin('multimedia','multimedia.fk_solicitud','=','solicitud.pk_solicitud')->toSql();
+        dd($solicitudes);
+        // dd(count($solicitudes));
+        return view('solicitudes.solicitudes', ['solicitudes' => $solicitudes, 'num' => count($solicitudes)]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create(){
         $r=UsuarioController::editSolicitante();
         if($r==null){
@@ -36,24 +28,126 @@ class SolicitudController extends Controller
         
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(SolicitudStoreController $request){
         $solicitud = (new Solicitud)->fill($request->all());
         $solicitud->fk_usuario= session('datos')["pk_usuario"];
+        $hoy = new DateTime();
+        $anos = $hoy->diff(session('datos')["fecha_nacimiento"]);
+        $edad=$annos->y;
+        if(session('datos')["empresa"]){
+            $tipo=3;
+            if($edad>1){
+                if($edad>3){
+                    $tiempo=2;
+                }else{
+                    $tiempo=4;
+                }
+            }else{
+                $tiempo=5;
+            }
+        }else{
+            $tipo=6;
+        }
+        if(session('datos')["activos"]>$request->monto_requerido){
+            $act=0;
+        }else{
+            if(session('datos')["activos"]< $request->monto_requerido/2){
+                $act=8;
+            }else{
+                $act=4;
+            }
+            
+        }
+        switch ($request->categoria) {
+            case "educacion":
+                $cat=1;
+                break;
+            case "investigacion":
+                $cat=1;
+                break;
+            case "arte":
+                $cat=6;
+                break;
+            case "empresa":
+                $cat=1;
+                break;
+            case "personal":
+                $cat=5;
+                break;
+        }
+
+
+        if(session('datos')["pasivos"] == 0 or session('datos')["activos"]==0){
+            $pa=8;
+        }else{
+            $pa=2;
+        }
+        if(strlen($request->descripcion)>180){
+            $des=4;
+        }else{
+            $des=8;
+        }
+        if($request->foto==null){
+            $fot=5;
+        }else{
+            $fot=1;
+        }
+        if($request->video==null){
+            $vid=5;
+        }else{
+            $vid=1;
+        }
+        switch (session('datos')["nivel"]) {
+            case "ninguno":
+                $niv=10;
+                break;
+            case "primaria":
+                $niv=9;
+                break;
+            case "bachiller":
+                $niv=8;
+                break;
+            case "tecnico":
+                $niv=7;
+                break;
+            case "tecnologo":
+                $niv=6;
+                break;
+            case "universitario":
+                $niv=5;
+                break;
+            case "maestria":
+                $niv=4;
+                break;
+            case "doctorado":
+                $niv=3;
+                break;
+        }
+
+        if($request->monto_requerido>1000000){
+            if($request->monto_requerido>100000000){
+                $mon=8;
+            }else{
+                $mon=4;
+            }
+        }else{
+            $mon=2;
+        }
+        $variables = [$tiempo,$tipo,$act,$pa,$pa,$cat,$des,$fot,$vid,$niv,$mon];
+        $solicitud->riesgo=calcular_var($variables);
         $solicitud->save();
         Multimedia::create([
             'fk_solicitud' => $solicitud->pk_solicitud,
             'tipo' => 'foto',
-            'descripcion' => $request->descripcion_video,
-            'url' => 
+            'descripcion' => $request->descripcion_foto,
+            'url' => SupraController::subirArchivo($request,'solicitud'.$solicitud->pk_solicitud,'foto'),
         ]);
-        
-        
+        Multimedia::create([
+            'fk_solicitud' => $solicitud->pk_solicitud,
+            'tipo' => 'video',
+            'descripcion' => $request->descripcion_video,
+            'url' => $request->video,
+        ]);        
     }
 
     /**
@@ -99,5 +193,17 @@ class SolicitudController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    private function calcular_var($variables){
+        $pesos=[3,5,3,3,5,3,1,1,5,5,5];
+        $total=39;
+        $i;
+        $riesgo = 0.0;
+        for ($i = 0; $i < 10; $i++) { 
+            $riesgo = $riesgo + $pesos[$i]*$valores[$i];
+        }
+        $riesgo=($riesgo/$total)*10;
+        return $riesgo;
     }
 }
